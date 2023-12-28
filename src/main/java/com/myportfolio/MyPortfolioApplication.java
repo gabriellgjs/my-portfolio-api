4200dae3bb41767513f3bfb4b47c60ff073425dd
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2023-12-27 21:20:47 -0300

    feat: start project

---
+++ src/main/java/com/myportfolio/MyPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.myportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myportfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
