1e063e1a7de386487b3142f36bf5c8479167bde1
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2024-01-01 13:27:18 -0300

    fix: unused import statement

--- src/main/java/com/myportfolio/education/repositories/EducationRepository.java
+++ src/main/java/com/myportfolio/education/repositories/EducationRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Repository
 public interface EducationRepository extends JpaRepository<Education, Long> {
