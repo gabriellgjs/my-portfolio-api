a57357b1b2f44092404ef01e291ec0dfec8ff333
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2024-01-17 21:29:46 -0300

    feat: new migrations

---
+++ src/main/resources/db/migration/V6__create_table_skills_and_first_many_to_many.sql
@@ -0,0 +1,12 @@
+CREATE TABLE skills (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE developers_skills (
+    developer_id INT NOT NULL,
+    skill_id INT NOT NULL,
+    PRIMARY KEY (developer_id, skill_id),
+    FOREIGN KEY (developer_id) REFERENCES developers (id),
+    FOREIGN KEY (skill_id) REFERENCES skills (id)
+);
