c801ae2bb46b67494a2f54a99f9204089d4b32eb
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2024-01-18 21:46:03 -0300

    refactor: never used

--- src/main/java/com/myportfolio/projects/controller/ProjectController.java
+++ src/main/java/com/myportfolio/projects/controller/ProjectController.java
@@ -5,7 +5,6 @@ import com.myportfolio.projects.models.Project;
 import com.myportfolio.projects.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
